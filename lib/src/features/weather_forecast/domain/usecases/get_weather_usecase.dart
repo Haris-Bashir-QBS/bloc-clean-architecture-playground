f7a837cf08f95c34519540d9a2e7db4af5300419
unknown <harisbashirqbs@gmail.com>
2025-03-12 12:33:34 +0500

    override weather use case

--- lib/src/features/weather_forecast/domain/usecases/get_weather_usecase.dart
+++ lib/src/features/weather_forecast/domain/usecases/get_weather_usecase.dart
@@ -1,14 +1,16 @@
+import 'package:bloc_api_integration/src/core/use_cases/use_case.dart';
 import 'package:bloc_api_integration/src/features/weather_forecast/domain/entities/weather_entity.dart';
 import 'package:fpdart/fpdart.dart';
 
 import '../../../../network/api_exceptions.dart';
 import '../repositories/weather_repository.dart';
 
-class GetWeatherUseCase {
+class GetWeatherUseCase implements UseCase<WeatherEntity, String> {
   final WeatherRepository repository;
 
   GetWeatherUseCase(this.repository);
 
+  @override
   Future<Either<Failure, WeatherEntity>> call(String cityName) {
     return repository.getWeather(cityName);
   }
